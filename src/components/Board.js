e29793af40f53342ea101701a941d7b1bb1a6489
Aaron Collins <aa.collins.95@gmail.com>
2022-06-16 10:58:42 -0400

    Initial Commit

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+
+function Board() {
+  return (
+    <div className="Board">
+      
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import logo from '../assets/logo.svg';
+import React, { useState } from 'react';
+import '../styles/Header.css';
+
+//import '../styles/App.css';
+
+const Header = (props) => {
+    const [fieldText, setFieldText] = useState('')
+    const current = 3;
+    const high = 5;
+    const fieldName = props.fieldName
+
+    return (
+        <div className="header">
+            <h1>Mixed Ultimate Memory Game</h1>
+            <div className="scoreboard">
+                <p>Current Score: {current} </p>
+                <p>High Score: {high} </p>
+            </div>
+           
+        </div>
+    )
+}
+
+export default Header;
